ce28715431374ad580660834ada8e39c7b34d950
杨彤 <18211308381@163.com>
2018-12-13 09:07:14 +0800

    no message

---
+++ SQLiteManager/SQLiteManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SQLiteManager
+//
+//  Created by 杨彤 on 2018/12/13.
+//  Copyright © 2018年 JJXT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
